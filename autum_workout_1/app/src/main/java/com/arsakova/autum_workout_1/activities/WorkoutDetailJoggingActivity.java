b0660fec6b3565c37cf373190fee96e24aae7eb9
Ekellya <tatiana.arsakova@gmail.com>
2018-10-14 14:49:46 +0400

    Добавлено логирование методов жизненного цикла Activity, на примере WorkoutDetailLiftingTheRodActivity применены методы onSaveInstanceState() и onRestoreInstanceState(), позволяющие сохранять информацию при пересоздании Activity (поворот экрана)

--- autum_workout_1/app/src/main/java/com/arsakova/autum_workout_1/activities/WorkoutDetailJoggingActivity.java
+++ autum_workout_1/app/src/main/java/com/arsakova/autum_workout_1/activities/WorkoutDetailJoggingActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,7 +19,7 @@ import java.util.Date;
 
 public class WorkoutDetailJoggingActivity extends AppCompatActivity {
 
-
+    public static final String TAG = "JoggingActivity";
     private TextView title;
     private TextView recordDate;
     private TextView description;
@@ -40,6 +41,43 @@ public class WorkoutDetailJoggingActivity extends AppCompatActivity {
         workout = new Workout("jogging","frrr" , 0, new Date(), 0);
         initGUI(workout);
         addListeners();
+        Log.d(TAG, "Вызван onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "Вызван onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Вызван onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Вызван onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Вызван onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "Вызван onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Вызван onDestroy()");
     }
 
     private void addListeners() {
--- autum_workout_1/app/src/main/java/com/arsakova/autum_workout_1/activities/WorkoutListActivity.java
+++ autum_workout_1/app/src/main/java/com/arsakova/autum_workout_1/activities/WorkoutListActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.arsakova.autum_workout_1.R;
 
 public class WorkoutListActivity extends AppCompatActivity {
+    public static final String TAG = "WorkoutListActivity";
     private Button buttonLiftingTheRod;
     private Button buttonPullUps;
     private Button buttonJogging;
@@ -18,19 +20,54 @@ public class WorkoutListActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_list);
-
         initGUI();
+        Log.d(TAG, "Вызван onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "Вызван onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Вызван onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Вызван onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Вызван onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "Вызван onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Вызван onDestroy()");
     }
 
     private void initGUI() {
         buttonLiftingTheRod = findViewById(R.id.lifting_the_rod_button);
         buttonPullUps = findViewById(R.id.pull_ups_button);
-//        buttonJogging = findViewById(R.id.jogging_button);
+        buttonJogging = findViewById(R.id.jogging_button);
 
         buttonPullUps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(WorkoutListActivity.this, "   ", Toast.LENGTH_SHORT).show();
                 Intent startWorkoutDetailPullUpsActivity = new Intent(WorkoutListActivity.this, WorkoutDetailPullUpsActivity.class);
                 startActivity(startWorkoutDetailPullUpsActivity);
             }
@@ -38,7 +75,6 @@ public class WorkoutListActivity extends AppCompatActivity {
         buttonLiftingTheRod.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(WorkoutListActivity.this, "   ", Toast.LENGTH_SHORT).show();
                 Intent startWorkoutDetailLiftingTheRodActivity = new Intent(WorkoutListActivity.this, WorkoutDetailLiftingTheRodActivity.class);
                 startActivity(startWorkoutDetailLiftingTheRodActivity);
             }
@@ -46,7 +82,6 @@ public class WorkoutListActivity extends AppCompatActivity {
         buttonJogging.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(WorkoutListActivity.this, "   ", Toast.LENGTH_SHORT).show();
                 Intent startWorkoutDetailLiftingTheRodActivity = new Intent(WorkoutListActivity.this, WorkoutDetailJoggingActivity.class);
                 startActivity(startWorkoutDetailLiftingTheRodActivity);
             }
